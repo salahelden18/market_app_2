7e9f03293925b0286384d64c2f65884929bd4b76
Shehab Mokhtar <shahbeldinmokhtar@gmail.com>
2024-05-22 20:41:01 +0300

    Search

--- lib/features/search/data/presentation/view/widgets/search_product_item_widget.dart
+++ lib/features/search/data/presentation/view/widgets/search_product_item_widget.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:market_app_2/core/style/app_colors.dart';
 import 'package:market_app_2/features/home/presentation/view_models/branch/branch_cubit.dart';
 import 'package:market_app_2/features/products/data/models/branch_product_model.dart';
+import 'package:market_app_2/features/products/presentation/views/product_details_screen.dart';
 
 class SearchProductItemWidget extends StatelessWidget {
   const SearchProductItemWidget({
@@ -13,39 +14,46 @@ class SearchProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final heroTag = 'product-hero-tag-${model.product!.id}';
     final String currencySymbol =
         context.read<BranchCubit>().state.branchModel!.currencySymbol!;
-    return Container(
-      padding: const EdgeInsetsDirectional.all(7),
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.primaryColor),
-        borderRadius: BorderRadiusDirectional.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Proddut image
-          Expanded(child: Image.network(model.product!.images[0])),
-          // Product name
-          Text(
-            model.product!.enName,
-            maxLines: 1,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, ProductDetailsScreen.routeName,
+            arguments: [model, heroTag, 0]);
+      },
+      child: Container(
+        padding: const EdgeInsetsDirectional.all(7),
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromARGB(255, 41, 41, 41)),
+          borderRadius: BorderRadiusDirectional.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Proddut image
+            Expanded(child: Image.network(model.product!.images[0])),
+            // Product name
+            Text(
+              model.product!.enName,
+              maxLines: 1,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 17,
+              ),
             ),
-          ),
-          // Product desc
-          Text(
-            model.product!.enDescription!,
-            maxLines: 1,
-          ),
-          // Product price
-          Text(
-            '${model.price} $currencySymbol',
-            maxLines: 1,
-          ),
-        ],
+            // Product desc
+            Text(
+              model.product!.enDescription!,
+              maxLines: 1,
+            ),
+            // Product price
+            Text(
+              '${model.price} $currencySymbol',
+              maxLines: 1,
+            ),
+          ],
+        ),
       ),
     );
   }
